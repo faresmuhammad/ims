e09353efb6f6097d009a68af3fe0af17c5c19b96
Fares Muhammad <eng-fares.muhammad1823@alexu.edu.eg>
2024-04-25 20:10:37 +0200

    Add relations to Order and OrderItem Eloquent

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -20,4 +20,9 @@ class Order extends Model
     {
         return $this->belongsTo(Supplier::class);
     }
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -10,4 +10,14 @@ class OrderItem extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
